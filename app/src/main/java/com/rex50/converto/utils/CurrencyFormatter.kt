f8075ad5f9960c2091c9cf98022bf8900d0157cc
Pavitra <pareshraut50@gmail.com>
2023-03-20 20:56:39 +0530

    fix: Show scientific expression when accuracy decreases

--- app/src/main/java/com/rex50/converto/utils/CurrencyFormatter.kt
+++ app/src/main/java/com/rex50/converto/utils/CurrencyFormatter.kt
@@ -1,22 +1,28 @@
 package com.rex50.converto.utils
 
 import java.text.DecimalFormat
+import java.util.*
 
 class CurrencyFormatter {
 
-    fun format(amount: Double?, currencyCode: String): String {
-        return DecimalFormat.getCurrencyInstance().let { currencyFormatter ->
+    fun format(amount: Double?, currencyCode: String): String =
+        DecimalFormat.getCurrencyInstance().let { currencyFormatter ->
+            val (currency, symbol) = getCurrency(currencyCode)
+            currencyFormatter.currency = currency
             currencyFormatter.maximumFractionDigits = 2
-            var symbol: String
-            java.util.Currency.getInstance(currencyCode).let {
-                currencyFormatter.currency = it
-                symbol = it.symbol
-            }
             amount?.let {
-                currencyFormatter.format(it)
-                    .replace(currencyCode, "")
+                val formattedValue = currencyFormatter.format(it)
+
+                // After 18 digits, accuracy decreases so use scientific expression
+                if (formattedValue.replace(symbol, "").length >= 18)
+                    "$symbol$amount"
+                else
+                    formattedValue.replace(currencyCode, "")
             } ?: "${symbol}0.00"
         }
-    }
 
+    private fun getCurrency(currencyCode: String): Pair<Currency, String> =
+        Currency.getInstance(currencyCode).let {
+            it to it.symbol.replace(currencyCode, "")
+        }
 }
